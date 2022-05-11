1687cf1afdbc20e90398961362dc60b4e1cd6106
Armando Lopez <lopez@Armandos-MBP.home>
2022-05-11 16:43:51 -0700

    Updates

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-//import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
-//import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
 //import { AmbientLight, Camera, Mesh } from 'three';
-
-
 
 let loader = new THREE.GLTFLoader();
 const scene = new THREE.Scene();
